73f66af1de046a81352309d288b4063c3421368f
Maxime Arnaudy <maxime.arnaudy@gmail.com>
2022-09-20 15:54:20 +0200

    Finish cpp00/ex02
    Add proper timestamp

--- cpp/cpp00/ex02/Account.cpp
+++ cpp/cpp00/ex02/Account.cpp
@@ -50,7 +50,7 @@ Account::~Account() {
 void Account::makeDeposit(int deposit) {
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex << ";p_amount:" << _amount;
-	std::cout << "deposit:" << deposit;
+	std::cout << ";deposit:" << deposit;
 	_amount += deposit;
 	_totalAmount += deposit;
 	_nbDeposits++;
@@ -61,7 +61,7 @@ void Account::makeDeposit(int deposit) {
 bool Account::makeWithdrawal(int withdrawal) {
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex << ";p_amount:" << _amount;
-	std::cout << "withdrawal:";
+	std::cout << ";withdrawal:";
 	if (withdrawal > _amount) {
 		std::cout << "refused" << std::endl;
 		return (false);
@@ -71,7 +71,7 @@ bool Account::makeWithdrawal(int withdrawal) {
 	_nbWithdrawals++;
 	_totalNbWithdrawals++;
 	std::cout << withdrawal << ";amount:" << _amount;
-	std::cout << withdrawal << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+	std::cout << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 	return (true);
 }
 
@@ -87,7 +87,24 @@ void Account::displayStatus() const {
 }
 
 void Account::_displayTimestamp() {
-	std::cout << "[" << std::time(NULL) << "]";
+	std::time_t time = std::time(NULL);
+	std::tm *l_time = localtime(&time);
+	std::cout << '[' << l_time->tm_year + 1900;
+	if (l_time->tm_mon < 10)
+		std::cout << '0';
+	std::cout << l_time->tm_mon + 1;
+	if (l_time->tm_mday < 10)
+		std::cout << '0';
+	std::cout << l_time->tm_mday << '_';
+	if (l_time->tm_hour < 10)
+		std::cout << '0';
+	std::cout << l_time->tm_hour;
+	if (l_time->tm_min < 10)
+		std::cout << '0';
+	std::cout << l_time->tm_min;
+	if (l_time->tm_sec < 10)
+		std::cout << '0';
+	std::cout << l_time->tm_sec << ']';
 }
 
 int Account::_nbAccounts = 0;
