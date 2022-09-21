e76d86cc6aedf6bde9cae69ef4b1fe8ffec24160
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2022-09-21 14:11:04 +0200

    Add string obj to cpp00/ex00

--- cpp/cpp00/ex00/megaphone.cpp
+++ cpp/cpp00/ex00/megaphone.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <string>
 
 int	main(int argc, char **argv) {
 	if (argc <= 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
+	std::string str;
 	for (int i = 1; argv[i]; i++) {
-		for (int j = 0; argv[i][j]; j++){
-			argv[i][j] = toupper(argv[i][j]);
+		str = std::string(argv[i]);
+		for (unsigned int j = 0; j < str.length(); j++){
+			str[j] = toupper(str[j]);
 		}
-		std::cout << argv[i];
+		std::cout << str;
 	}
 	std::cout << std::endl;
 }
