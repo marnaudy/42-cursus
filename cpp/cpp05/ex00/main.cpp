c5475b3d8f8ab9db1590c4a155b7e70e2d4d029b
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2022-12-01 09:11:18 +0100

    Cpp5 finish ex0

---
+++ cpp/cpp05/ex00/main.cpp
@@ -0,0 +1,50 @@
+#include "Bureaucrat.hpp"
+
+int main() {
+	try {
+		Bureaucrat bob(1, "Bob");
+		std::cout << bob << std::endl;
+		Bureaucrat bob2 = bob;
+		std::cout << bob2 << std::endl;
+		bob2.decrementGrade();
+		std::cout << bob2 << std::endl;
+		bob.incrementGrade();
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+	try {
+		Bureaucrat bill(150, "Bill");
+		std::cout << bill << std::endl;
+		Bureaucrat bill2 = bill;
+		std::cout << bill2 << std::endl;
+		bill2.incrementGrade();
+		std::cout << bill2 << std::endl;
+		bill.decrementGrade();
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+	try	{
+		std::cout << "Trying grade 0" << std::endl;
+		Bureaucrat billybob(0, "Billybob");
+		std::cout << billybob << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+	try	{
+		std::cout << "Trying grade 151" << std::endl;
+		Bureaucrat billybob(151, "Billybob");
+		std::cout << billybob << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << "Testing name overwrite" << std::endl;
+	const std::string name = "Alice the third, haver of long name";
+	Bureaucrat alice(1, name);
+	Bureaucrat bob(1, "Bob");
+	bob = alice;
+	std::cout << bob << std::endl;
+}
